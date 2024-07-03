718b98d8f6f58cec85516e9b80bbff393e52151e
ManishDhamala <dhamalamanish32@gmail.com>
2024-07-03 22:48:38 +0545

    Setting model class

---
+++ src/main/java/com/project/demo/model/Role.java
@@ -0,0 +1,30 @@
+package com.project.demo.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+
+
+}
