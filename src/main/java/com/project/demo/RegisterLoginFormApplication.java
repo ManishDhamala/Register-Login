bd5dbfe3c8261fcc08bd2af70058115931ce9d11
ManishDhamala <dhamalamanish32@gmail.com>
2024-07-03 22:14:56 +0545

    Initial commit

---
+++ src/main/java/com/project/demo/RegisterLoginFormApplication.java
@@ -0,0 +1,15 @@
+package com.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterLoginFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterLoginFormApplication.class, args);
+
+        System.out.println("Hello World");
+    }
+
+}
